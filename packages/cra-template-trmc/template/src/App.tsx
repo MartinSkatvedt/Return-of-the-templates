65a6d37751232a4bbd7b5a6e1eb304e886406324
Martin Skatvedt <martinskatvedt@gmail.com>
2021-11-23 00:55:21 +0100

    Small change to trigger publish

--- packages/cra-template-trmc/template/src/App.tsx
+++ packages/cra-template-trmc/template/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "@chakra-ui/core";
 function App() {
-  return <Box className="App"></Box>;
+  return <Box className="App">Welcome!</Box>;
 }
 
 export default App;
